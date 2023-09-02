a971ea7de46a3e33809ac78f63f0c2d1e99776b3
Griotold <gotjd9773@naver.com>
2023-09-02 19:00:12 +0900

    one day event

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,19 +1,30 @@
 import event.Event;
+import event.OneDayEvent;
+import util.MyDate;
 
 import java.util.Scanner;
 
+
 public class Scheduler {
 
-    Event[] events = new Event[100];
-
+    public Event[] events = new Event[100];
+    public int n = 0;
+    private Scanner kb;
     public void processCommand() {
 
-        Scanner kb = new Scanner(System.in);
+        kb = new Scanner(System.in);
         while(true) {
-            System.out.print("$");
+            System.out.print("$ ");
             String command = kb.next();
             if (command.equals("addevent")) {
-
+                String type = kb.next();
+                if (type.equalsIgnoreCase("oneday")) {
+                    handleAddOneDayEvent();
+                } else if (type.equalsIgnoreCase("duration")) {
+                    handleAddDurationEvent();
+                } else if (type.equalsIgnoreCase("deadline")) {
+                    handleAddDeadlineEvent();
+                }
             } else if (command.equals("list")) {
 
             } else if (command.equals("show")) {
@@ -24,6 +35,40 @@ public class Scheduler {
         }
         kb.close();
     }
+
+    private void handleAddOneDayEvent() {
+        System.out.print("   when: ");
+        String dateString = kb.next();
+
+        System.out.print("   title: ");
+        String title = kb.next();
+
+        MyDate date = customParseDateString(dateString);
+
+        OneDayEvent oneDayEvent = new OneDayEvent(title, date);
+
+        System.out.println(oneDayEvent);
+        events[n++] = oneDayEvent;
+    }
+
+    private MyDate customParseDateString(String dateString) {
+        String[] tokens = dateString.split("/");
+
+        int year = Integer.parseInt(tokens[0]);
+        int month = Integer.parseInt(tokens[1]);
+        int day = Integer.parseInt(tokens[2]);
+
+        return new MyDate(year, month, day);
+    }
+
+    private void handleAddDurationEvent() {
+
+    }
+
+    private void handleAddDeadlineEvent() {
+
+    }
+
     public static void main(String[] args) {
         Scheduler app = new Scheduler();
         app.processCommand();
