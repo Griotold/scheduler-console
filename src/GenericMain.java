56e8f1380b89fe5bc74a0117da4e9bd24f21c1fb
Griotold <gotjd9773@naver.com>
2023-09-17 15:21:10 +0900

    제네릭의 필요성 - 제네릭을 안 쓰면 직접 형변환해줘야하는 번거로움이 생긴다.

--- src/GenericMain.java
+++ src/GenericMain.java
@@ -1,6 +1,7 @@
 import genericexercise.GenericPrinter;
 import genericexercise.Plastic;
 import genericexercise.Powder;
+import genericexercise.PrinterV1;
 
 public class GenericMain {
 
@@ -16,5 +17,11 @@ public class GenericMain {
         plasticPrinter.setMaterial(new Plastic());
         Plastic plastic = plasticPrinter.getMaterial();
         System.out.println(plasticPrinter);
+
+        // 제네릭을 사용하지 않은 프린터는 직접 형변환해줘야하는 번거로움이 생긴다.
+        PrinterV1 printerV1 = new PrinterV1();
+        Powder p1 = new Powder();
+        printerV1.setMaterial(p1); // 여기는 자동 형변환!
+        Powder material = (Powder) printerV1.getMaterial(); // 여기는 직접 형변환
     }
 }
---
+++ src/genericexercise/PrinterV1.java
@@ -0,0 +1,16 @@
+package genericexercise;
+/**
+ * 제네릭을 사용하지 않은 3D 프린터
+ * */
+public class PrinterV1 {
+
+    private Object material;
+
+    public Object getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Object material) {
+        this.material = material;
+    }
+}
