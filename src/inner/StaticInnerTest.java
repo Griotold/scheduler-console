7dd6fd540f715827eaedf14ac91b0797cf5ed865
Griotold <gotjd9773@naver.com>
2023-09-21 12:44:44 +0900

    내부 클래스 - 정적 내부 클래스

---
+++ src/inner/StaticInnerTest.java
@@ -0,0 +1,33 @@
+package inner;
+class OutClass2 {
+    private int num = 10;
+    private static int sNum = 20;
+    static class InStaticClass {
+        int inNum = 100;
+        static int sInNum = 200;
+
+        void inTest() {
+            // num += 10; // 외부 클래스의 인스턴스 변수는 사용할 수 없다.
+            System.out.println("InStaticClass inNum = " + inNum + "(내부 클래스의 인스턴스 변수 사용)");
+            System.out.println("InStaticClass sInNum = " + sInNum + "(내부 클래스의 정적 변수 사용)");
+            System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 정적 변수 사용)");
+        }
+
+        static void sTest() {
+            // num += 10; // 외부 클래스와 내부 클래스의 인스턴스 변수는 사용할 수 없다.
+            // inNum += 10;
+            System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 정적 변수 사용)");
+            System.out.println("InStaticClass sInNum = " + sInNum + "(내부 클래스의 정적 변수 사용)");
+        }
+    }
+}
+public class StaticInnerTest {
+    public static void main(String[] args) {
+        OutClass2.InStaticClass sInClass = new OutClass2.InStaticClass();
+        System.out.println("정적 내부 클래스 일반 메소드 호출");
+        sInClass.inTest();
+        System.out.println("------------");
+        System.out.println("정적 내부 클래스의 정적 메소드 호출");
+        OutClass2.InStaticClass.sTest();
+    }
+}
